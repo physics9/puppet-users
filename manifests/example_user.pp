4b2de986d0ea376ee63456b16449449b51032b65
Jovana Ciric <jovanaciric@MacBookPro121-jovanaciric.local>
2018-07-16 10:30:55 +0200

    Tested and is working

--- manifests/example_user.pp
+++ manifests/example_user.pp
@@ -2,9 +2,7 @@ class users::example {
 users::virtual { 'example':
 ensure            => present,
 uid               => 100x,
+gid               => 100x,
 email             => 'example@gmail.com',
-sshkeytype        => 'ssh-rsa',
-sshkey            =>
-' Here input the users ssh-key'
 }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,56 +1,50 @@
-define users::virtual () {
+define users::virtual ($ensure, $uid, $gid, $email) {
+
 include users::params
 $homepath  = $users::params::homepath
 $shell     = $users::params::shell
 
-#create the user
-user {$name:
-ensure            =>  $ensure,
-uid               =>  $uid,
-gid               =>  $gid,
-shell             =>  $shell,
-home              =>  "${homepath}/${name}",
-comment           =>  $email,
-managehome        =>  true,
-require           =>  Group[$name],
+#make a group
+group {$name:
+  ensure            => $ensure,
+  gid               => $gid
 }
 
-# Ensure the home directory exists with the right permissions
+#make a user
+user {$name:
+  ensure            =>  $ensure,
+  uid               =>  $uid,
+  gid               =>  $gid,
+  shell             =>  $shell,
+  home              =>  "${homepath}/${name}",
+  comment           =>  $email,
+  managehome        =>  true,
+  require           =>  Group[$name],
+}
+
 File { "${homepath}/${name}":
-ensure            =>  directory,
-owner             =>  $name,
-group             =>  $name,
-mode              =>  '0700',
-require           =>  [ User[$name], Group[$name] ],
+  ensure            =>  directory,
+  owner             =>  $name,
+  group             =>  $name,
+  mode              =>  '0700',
+  require           =>  [ User[$name], Group[$name] ],
 }
 
 file { "${homepath}/${name}/.ssh":
-ensure            =>  directory,
-owner             =>  $name,
-group             =>  $name,
-mode              =>  '0700',
-require           =>  [ User[$name], Group[$name] ],
+  ensure            =>  directory,
+  owner             =>  $name,
+  group             =>  $name,
+  mode              =>  '0700',
+  require           =>  [ User[$name], Group[$name] ],
 }
 
-# Add user's SSH key
-ssh_authorized_key { $email:
-ensure           => present,
-name             => $name,
-user             => $name,
-type             => $sshkeytype,
-key              => $sshkey,
-}
 
 file { "${homepath}/${name}/.ssh/authorized_keys":
-ensure            =>  present,
-content           =>  'puppet:///modules/users/ssh_keys,
-owner             =>  $name,
-group             =>  $admin,
-mode              =>  '0640',
-require           =>  [ User[$name], Group[$admin] ],
+  ensure            =>  present,
+  source            =>  "puppet:///modules/users/$name",
+  owner             =>  $name,
+  group             =>  $name,
+  mode              =>  '0640',
+  require           =>  [ User[$name], Group[$name] ],
 }
 }
-
-
-
-
